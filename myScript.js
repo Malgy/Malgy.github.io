49c05a5ea4dd5d9dabfb7d34521e0a21f6c7ab98
Anthony Malgapo <anthony.malgapo@gmail.com>
2017-05-19 16:51:03 -0400

    trying to make the mouseover work with my page

--- myScript.js
+++ myScript.js
@@ -1,10 +1,10 @@
 //Using jQuery with my website
 $(document).ready(function(){
-	$a = $('a');
+	$a = $('img');
 	$a.mouseover(function(){
-		$a.fadeTo('fast', 1);
+		$a.fadeTo('fast', 0.5);
 	});
 	$a.mouseout(function(){
-		$a.fadeTo('fast', 0.5);
+		$a.fadeTo('fast', 1);
 	});
 });
